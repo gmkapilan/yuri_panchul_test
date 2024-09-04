8b532534f36739ca60e2fa0dc62fdf626e9aec5d
gmkapilan <160381275+gmkapilan@users.noreply.github.com>
2024-09-03 17:00:21 -0700

    Update project.sv

--- src/project.sv
+++ src/project.sv
@@ -10,7 +10,7 @@ module tt_um_yuri_panchul_adder_with_flow_control
     input  [7:0] ui_in,    // Dedicated inputs
     output [7:0] uo_out,   // Dedicated outputs
     input  [7:0] uio_in,   // IOs: Input path
-    output [7:0] uio_out,  // IOs: Output path
+    output [7:0] uio_out,  // IOs: Output path 
     output [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input        ena,      // always 1 when the design is powered, so you can ignore it
     input        clk,      // clock
